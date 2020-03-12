9f8a0e2b91e255a879b85e363531cd51db21996d
Memitaru <amiezrascott@gmail.com>
2020-03-11 20:34:41 -0700

    Added lecture notes

--- lecture.py
+++ lecture.py
@@ -5,4 +5,30 @@ class DynamicArray:
         self.storage = [None] * capacity
 
     def insert(self, index, value):
+
+        if self.count == self.c1apacity:
+            self.double_size()
+        
+        for idx in range(self.count, index, -1):
+            self.storage[idx] = self.storage[idx-1]
+
+        self.storage[index] = value
+        self.count += 1
+
+    def append(self, value):
+        if self.count == self.c1apacity:
+            self.double_size()
+
+        self.storage[self.count] = value
+
+        self.count += 1
+
+    def double_size(self):
+        self.capacity *= 2
+        new_arr = [None] * self.capacity
+
+        for i in range(self.count):
+            new_arr[i] = self.storage[i]
+
+        self.storage = new_arr
         
